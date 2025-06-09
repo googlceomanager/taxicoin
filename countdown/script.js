e184d552e10671215318c9da63abc930bf052951
googlceomanager <googl.ceo.manager@gmail.com>
2025-06-10 00:12:33 +0100

    commit

--- countdown/script.js
+++ countdown/script.js
@@ -11,7 +11,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "6/28/",
+      dayMonth = "7/12/",
       birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
